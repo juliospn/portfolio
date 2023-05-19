9cb4722ae16688f6b409723f9265616ffb5557ac
juliospn <116590563+juliospn@users.noreply.github.com>
2023-05-19 13:35:30 -0300

    Delete portfolio directory

--- portfolio/js/animation.js
+++
@@ -1,8 +0,0 @@
-var animationFile = '../assets/business-team.json';
-var animData = {
-  renderer: 'svg/canvas/html',
-  loop: true,
-  autoplay: true,
-  animationData: animationFile
-};
-var anim = bodymovin.loadAnimation(animData);
